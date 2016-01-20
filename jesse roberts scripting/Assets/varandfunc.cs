13ae660226f6d664aea46fe343ff2e55414cd117
unknown <10629434@CS514-17.ad.uvu.edu>
2016-01-20 09:17:30 -0700

    unity stuff var and func

---
+++ jesse roberts scripting/Assets/varandfunc.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class varandfunc : MonoBehaviour 
+{
+
+
+		
+		int myInt = 10;
+		
+		// Use this for initialization
+		void Start () 
+		{
+			myInt = MultiplyByTwo (myInt);
+			Debug.Log (myInt);
+		}
+		
+		int MultiplyByTwo (int number)
+		{
+			int ret;
+			ret = number * 2;
+			return ret;
+			
+			
+		}
+		
+		
+		// Update is called once per frame
+		void Update () 
+		{
+			
+		}
+	}
