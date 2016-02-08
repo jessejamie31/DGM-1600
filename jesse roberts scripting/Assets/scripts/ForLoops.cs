8f702b5a02a067ad76e6636d36030972cbb9e2be
unknown <10629434@CS514-19.ad.uvu.edu>
2016-02-08 09:36:50 -0700

    loops

---
+++ jesse roberts scripting/Assets/scripts/ForLoops.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForLoops : MonoBehaviour 
+{
+	int civiliansToSave = 3;
+
+	// Use this for initialization
+	void Start () 
+	{
+		while(civiliansToSave > 0)
+		{
+			Debug.Log ("You've saved a civilian!");
+			civiliansToSave--;
+	}
+	
+	// Update is called once per frame
+	//void Update () {
+	
+	}
+}
---
+++ jesse roberts scripting/Assets/scripts/WhileLoop.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WhileLoop : MonoBehaviour 
+{
+	int civiliansToSave = 3;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		while(civiliansToSave > 0)
+		{
+			Debug.Log ("You've saved a civilian!");
+			civiliansToSave--;
+		}
+		
+		// Update is called once per frame
+		//void Update () {
+		
+	}
+}
+
+
